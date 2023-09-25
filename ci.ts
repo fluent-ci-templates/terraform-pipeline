1f7556be7110373de2a6e278ec7cddf4cc77faac
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-25 06:11:09 +0000

    use deno v1.37

--- ci.ts
+++ ci.ts
@@ -1,12 +1,18 @@
-const command = new Deno.Command(Deno.execPath(), {
-  args: [
-    "run",
-    "-A",
-    "--import-map=https://deno.land/x/base_pipeline/import_map.json",
-    "https://deno.land/x/base_pipeline/src/dagger/runner.ts",
-  ],
-});
+import Client, { connect } from "https://sdk.fluentci.io/v0.1.9/mod.ts";
+import {
+  init,
+  validate,
+  plan,
+  apply,
+} from "https://pkg.fluentci.io/terraform_pipeline@v0.3.2/mod.ts";
 
-const { stdout } = await command.output();
+function pipeline(src = ".") {
+  connect(async (client: Client) => {
+    await init(client, src);
+    await validate(client, src);
+    await plan(client, src);
+    // await apply(client, src);
+  });
+}
 
-console.log(new TextDecoder().decode(stdout));
+pipeline();
