93123fd3165e5355761f02ce8416afc3739ae368
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-03-04 06:48:11 +0000

    update module exports

--- src/dagger/index.ts
+++ src/dagger/index.ts
@@ -1,4 +1,3 @@
-import pipeline from "./pipeline.ts";
 import { init, validate, plan, apply } from "./jobs.ts";
 
-export { pipeline, init, validate, plan, apply };
+export { init, validate, plan, apply };
