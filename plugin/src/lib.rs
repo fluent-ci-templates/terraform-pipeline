506b84aec1812e16236a84e9aae75a144bd2f5d8
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-11 06:50:44 +0000

    [plugin] allow custom terraform version

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -3,40 +3,84 @@ use fluentci_pdk::dag;
 
 #[plugin_fn]
 pub fn init(args: String) -> FnResult<String> {
+    let mut tf_version = dag().get_env("TF_VERSION").unwrap_or_default();
+
+    if tf_version.is_empty() {
+        tf_version = "latest".to_string();
+    }
+
     let stdout = dag()
+        .pipeline("init")?
         .pkgx()?
-        .with_packages(vec!["terraform"])?
-        .with_exec(vec!["terraform", "init", &args])?
+        .with_exec(vec![
+            "pkgx",
+            &format!("terraform@{}", tf_version),
+            "init",
+            &args,
+        ])?
         .stdout()?;
     Ok(stdout)
 }
 
 #[plugin_fn]
 pub fn validate(args: String) -> FnResult<String> {
+    let mut tf_version = dag().get_env("TF_VERSION").unwrap_or_default();
+
+    if tf_version.is_empty() {
+        tf_version = "latest".to_string();
+    }
+
     let stdout = dag()
+        .pipeline("validate")?
         .pkgx()?
-        .with_packages(vec!["terraform"])?
-        .with_exec(vec!["terraform", "validate", &args])?
+        .with_exec(vec![
+            "pkgx",
+            &format!("terraform@{}", tf_version),
+            "validate",
+            &args,
+        ])?
         .stdout()?;
     Ok(stdout)
 }
 
 #[plugin_fn]
 pub fn plan(args: String) -> FnResult<String> {
+    let mut tf_version = dag().get_env("TF_VERSION").unwrap_or_default();
+
+    if tf_version.is_empty() {
+        tf_version = "latest".to_string();
+    }
+
     let stdout = dag()
+        .pipeline("plan")?
         .pkgx()?
-        .with_packages(vec!["terraform"])?
-        .with_exec(vec!["terraform", "plan", &args])?
+        .with_exec(vec![
+            "pkgx",
+            &format!("terraform@{}", tf_version),
+            "plan",
+            &args,
+        ])?
         .stdout()?;
     Ok(stdout)
 }
 
 #[plugin_fn]
 pub fn apply(args: String) -> FnResult<String> {
+    let mut tf_version = dag().get_env("TF_VERSION").unwrap_or_default();
+
+    if tf_version.is_empty() {
+        tf_version = "latest".to_string();
+    }
+
     let stdout = dag()
+        .pipeline("apply")?
         .pkgx()?
-        .with_packages(vec!["terraform"])?
-        .with_exec(vec!["terraform", "apply", &args])?
+        .with_exec(vec![
+            "pkgx",
+            &format!("terraform@{}", tf_version),
+            "apply",
+            &args,
+        ])?
         .stdout()?;
     Ok(stdout)
 }
